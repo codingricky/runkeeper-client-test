29762ddac599223e25bee9a546a7771e4cf2d60d
Ricky <ricky@dius.com.au>
2013-09-17 20:29:57 +1000

    added more post deployment actions

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import com.github.codingricky.runkeeperclient.Authorisation;
 import play.*;
 import play.mvc.*;
 
@@ -16,6 +17,13 @@ public class Application extends Controller {
     }
 
     public static Result authorized() {
+        String code = request().getQueryString("code");
+        Configuration configuration = Play.application().configuration();
+        String clientId = configuration.getString("rk.clientId");
+        String clientSecret = configuration.getString("rk.clientSecret");
+        String redirectUri = configuration.getString("rk.redirectUrl");
+
+        new Authorisation().convertToken(code, clientId, clientSecret, redirectUri);
         return ok(authorized.render());
     }
   
